391726824b5e7e7b086acae1f68a9f26dd8f603e
Shedit <pyrt.91@gmail.com>
2020-03-22 23:04:25 -0400

    first commit

---
+++ API/call_functions.py
@@ -0,0 +1,35 @@
+import requests 
+import os
+
+
+class calls():
+
+    def __init__(self):
+        self.URL = "https://corona.lmao.ninja"
+
+    def historical(self, country = ''):
+
+        URL = self.URL + "/historical"
+        
+        if country == '':  
+            r = requests.get(url = URL)
+            return r.text
+
+        else:
+            # Because params in r.get delimit with '?' instead and REST api does not accept '?' only '/'
+            PATH = os.path.join (URL, country)
+            r = requests.get(url = PATH)
+            return r.text
+
+    def all(self):
+
+        URL = self.URL + "/all"
+        r = requests.get(url = URL)
+        return r.text
+
+    def countries(self):
+
+        URL = self.URL + "/countries"
+        r = requests.get(url = URL)
+        return r.text
+        
---
+++ README.md
@@ -0,0 +1,14 @@
+# Corona Virus lve updates. 
+
+## Intro
+
+This is an appliation that automatically gets updates about corona virus cases everyday. 
+
+## Goals
+
+- Create an API.
+- Present it in a decent way.
+- Automate it. 
+
+# API 
+
